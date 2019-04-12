99b3734067721e5c0c3c0a1b8a151083bccd9811
vladfreel <vladfreelmaster@gmail.com>
2019-04-12 10:30:29 +0300

    using graphql

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    uri: "/graphql"
+});
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root')
+);
+
+// How apollo can help us query data from the server
+client
+    .query({
+        query: gql `
+      {
+        authors {
+          name
+          books {
+            title
+          }
+        }
+      }
+    `
+    })
+    .then(result => console.log(result));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
